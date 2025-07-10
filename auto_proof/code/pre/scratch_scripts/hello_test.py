26b7ee16756893710d3cec3a5906e5a5c022dcfc
Shawn Stanley <shawns28@uw.edu>
2025-07-09 21:13:01 -0700

    removed scratch scripts

--- auto_proof/code/pre/scratch_scripts/hello_test.py
+++
@@ -1,7 +0,0 @@
-from auto_proof.code.pre import data_utils
-
-import numpy as np
-
-print("hello")
-roots = data_utils.load_txt("/allen/programs/celltypes/workgroups/rnaseqanalysis/shawn.stanley/auto_proof/auto_proof/auto_proof/test_data/roots_343_1300/split_359227/val_conf_no_error_in_box_roots.txt")
-print(len(roots))
--- auto_proof/code/pre/scratch_scripts/testing_cv.py
+++
@@ -1,9 +0,0 @@
-from cloudvolume import CloudVolume
-
-root = 864691135463333789
-seg_path = "graphene://middleauth+https://minnie.microns-daf.com/segmentation/table/minnie3_v1"
-cv_seg = CloudVolume(seg_path, progress=False, use_https=True, parallel=True)
-mesh = cv_seg.mesh.get(root, deduplicate_chunk_boundaries=False, remove_duplicate_vertices=False)
-print(mesh)
-mesh = mesh[root]
-print(mesh)
